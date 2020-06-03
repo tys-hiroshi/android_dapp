11cc28ceb1d60639e303edd25ae2fdfdc498ea06
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-06-03 21:45:06 +0900

    add FileUploadActivity

---
+++ dapp/app/src/main/java/com/hblockth/dapp/viewmodels/FileUploadActivity.kt
@@ -0,0 +1,12 @@
+package com.hblockth.dapp.viewmodels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FileUploadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_upload)
+    }
+}
